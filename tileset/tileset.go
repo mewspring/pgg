efd491d7bf5993598abdbe82cd2c9a4442040510
mewmew <rnd0x00@gmail.com>
2014-01-04 01:05:00 +0100

    tileset: Add package tileset which handles collections of one or more tile images.

---
+++ tileset/tileset.go
@@ -0,0 +1,75 @@
+// Package tileset handles collections of one or more tile images.
+package tileset
+
+import (
+	"image"
+
+	"github.com/mewkiz/pkg/imgutil"
+)
+
+// A TileSet is a collection of one or more tile images.
+type TileSet struct {
+	// Tileset sprite sheet.
+	imgutil.SubImager
+	// Tileset width.
+	width int
+	// Tileset height.
+	height int
+	// Tile width.
+	tileWidth int
+	// Tile height.
+	tileHeight int
+}
+
+// New returns a tile set based on the provided sprite sheet img.
+func New(img image.Image, tileWidth, tileHeight int) (ts *TileSet) {
+	ts = &TileSet{
+		tileWidth:  tileWidth,
+		tileHeight: tileHeight,
+	}
+	ts.SubImager = imgutil.SubFallback(img)
+	bounds := ts.Bounds()
+	ts.width = bounds.Dx()
+	ts.height = bounds.Dy()
+	return ts
+}
+
+// Open opens the sprite sheet specified by imgPath and returns a tile set based
+// on it.
+func Open(imgPath string, tileWidth, tileHeight int) (ts *TileSet, err error) {
+	img, err := imgutil.ReadFile(imgPath)
+	if err != nil {
+		return nil, err
+	}
+	ts = New(img, tileWidth, tileHeight)
+	return ts, nil
+}
+
+// A TileID uniquely identifies a tile image. The zero value represents no tile
+// image.
+type TileID int
+
+// tileRect returns the bounding rectangle in the sprite sheet of the tile image
+// specified by id.
+func (ts *TileSet) tileRect(id TileID) image.Rectangle {
+	tsCols := ts.width / ts.tileWidth
+	i := int(id - 1)
+	col := i % tsCols
+	row := i / tsCols
+	x := col * ts.tileWidth
+	y := row * ts.tileHeight
+	return image.Rect(x, y, x+ts.tileWidth, y+ts.tileHeight)
+}
+
+// Tile returns the tile image specified by id from the tile set.
+func (ts *TileSet) Tile(id TileID) image.Image {
+	rect := ts.tileRect(id)
+	return ts.SubImage(rect)
+}
+
+// TileCount returns the number of tiles contained within the tile set.
+func (ts *TileSet) TileCount() int {
+	tsCols := ts.width / ts.tileWidth
+	tsRows := ts.height / ts.tileHeight
+	return tsCols * tsRows
+}
