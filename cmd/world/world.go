d1552f0f96ffe74c1af86317e41e7c8de0e34f89
mewmew <rnd0x00@gmail.com>
2014-01-05 16:55:16 +0100

    cmd/world: Make use of new version of view and put draw logic in a closure.

--- cmd/world/world.go
+++ cmd/world/world.go
@@ -3,7 +3,6 @@ package main
 
 import (
 	"image"
-	"image/color"
 	"image/draw"
 	"log"
 
@@ -13,6 +12,12 @@ import (
 	"github.com/mewmew/pgg/view"
 )
 
+func init() {
+	// Specify the width and height of grid cells.
+	grid.CellWidth = 48
+	grid.CellHeight = 48
+}
+
 func main() {
 	err := world()
 	if err != nil {
@@ -20,62 +25,64 @@ func main() {
 	}
 }
 
-// Tile width and height in the tile set.
+// Number of columns and rows of the entire map.
 const (
-	TileWidth  = 32
-	TileHeight = 32
+	MapCols = 9
+	MapRows = 11
 )
 
 // Tile identifiers.
 const (
-	Water tileset.TileID = 1
-	Dirt  tileset.TileID = 2
-	Grass tileset.TileID = 3
-	Sand  tileset.TileID = 4
+	Grass  tileset.TileID = 1
+	Sand   tileset.TileID = 2
+	Water  tileset.TileID = 3
+	Gravel tileset.TileID = 4
 )
 
 // world initializes and renders the game world.
 func world() (err error) {
 	// Initialize map.
-	m := grid.NewMap(15, 15)
-
+	m := grid.NewMap(MapCols, MapRows)
 	initLevel(m)
 
-	// Initialize camera.
-	cam := view.Camera{
-		TopLeft: grid.Loc(1, 1),
-		Cols:    7,
-		Rows:    7,
-		Offset:  image.Pt(16, 16),
-	}
+	// Initialize view.
+	viewCols := 6
+	viewRows := 6
+	width := viewCols * grid.CellWidth
+	height := viewRows * grid.CellHeight
+	mapWidth := MapCols * grid.CellWidth
+	mapHeight := MapRows * grid.CellHeight
+	end := image.Pt(mapWidth, mapHeight)
+	v := view.NewView(width, height, end)
+
+	// Initialize world image.
+	world := image.NewRGBA(image.Rect(0, 0, width, height))
 
 	// Initialize tileset.
-	ts, err := tileset.Open("tileset 1.png", TileWidth, TileHeight)
+	tileWidth := grid.CellWidth
+	tileHeight := grid.CellHeight
+	ts, err := tileset.Open("tileset 2.png", tileWidth, tileHeight)
 	if err != nil {
 		return err
 	}
 
-	// Initialize world image.
-	width := TileWidth * cam.Cols
-	height := TileHeight * cam.Rows
-	world := image.NewRGBA(image.Rect(0, 0, width, height))
-
-	// Make background color lime.
-	lime := image.NewUniform(color.RGBA{0x00, 0xFF, 0x00, 0xFF})
-	draw.Draw(world, world.Bounds(), lime, image.ZP, draw.Over)
+	// drawTile draws the tile at the specified column and row.
+	drawTile := func(col, row int) {
+		x := col*grid.CellWidth - v.X()
+		y := row*grid.CellHeight - v.Y()
+		dr := image.Rect(x, y, x+tileWidth, y+tileHeight)
+		viewCol := col + v.Col()
+		viewRow := row + v.Row()
+		id := tileset.TileID(m[viewCol][viewRow])
+		tile := ts.Tile(id)
+		sp := tile.Bounds().Min
+		draw.Draw(world, dr, tile, sp, draw.Over)
+	}
 
 	// Draw loop.
-	for col := 0; col < cam.Cols; col++ {
-		for row := 0; row < cam.Rows; row++ {
-			camCol := col + cam.TopLeft.Col
-			camRow := row + cam.TopLeft.Row
-			id := tileset.TileID(m[camCol][camRow])
-			tile := ts.Tile(id)
-			xmin := col*TileWidth - cam.Offset.X
-			ymin := row*TileHeight - cam.Offset.Y
-			dr := image.Rect(xmin, ymin, xmin+TileWidth, ymin+TileHeight)
-			sp := tile.Bounds().Min
-			draw.Draw(world, dr, tile, sp, draw.Over)
+	for col := 0; col < v.Cols(); col++ {
+		for row := 0; row < v.Rows(); row++ {
+			drawTile(col, row)
 		}
 	}
 
@@ -132,10 +139,10 @@ func initLevel(m grid.Map) {
 	// Col 3.
 	m[3][0] = grid.Cell(Sand)
 	m[3][1] = grid.Cell(Sand)
-	m[3][2] = grid.Cell(Dirt)
-	m[3][3] = grid.Cell(Dirt)
-	m[3][4] = grid.Cell(Dirt)
-	m[3][5] = grid.Cell(Dirt)
+	m[3][2] = grid.Cell(Gravel)
+	m[3][3] = grid.Cell(Gravel)
+	m[3][4] = grid.Cell(Gravel)
+	m[3][5] = grid.Cell(Gravel)
 	m[3][6] = grid.Cell(Sand)
 	m[3][7] = grid.Cell(Sand)
 	m[3][8] = grid.Cell(Sand)
@@ -144,53 +151,66 @@ func initLevel(m grid.Map) {
 
 	// Col 4.
 	m[4][0] = grid.Cell(Sand)
-	m[4][1] = grid.Cell(Dirt)
-	m[4][2] = grid.Cell(Dirt)
-	m[4][3] = grid.Cell(Dirt)
-	m[4][4] = grid.Cell(Dirt)
-	m[4][5] = grid.Cell(Dirt)
-	m[4][6] = grid.Cell(Dirt)
-	m[4][7] = grid.Cell(Dirt)
+	m[4][1] = grid.Cell(Gravel)
+	m[4][2] = grid.Cell(Gravel)
+	m[4][3] = grid.Cell(Gravel)
+	m[4][4] = grid.Cell(Gravel)
+	m[4][5] = grid.Cell(Gravel)
+	m[4][6] = grid.Cell(Gravel)
+	m[4][7] = grid.Cell(Gravel)
 	m[4][8] = grid.Cell(Sand)
 	m[4][9] = grid.Cell(Sand)
 	m[4][10] = grid.Cell(Water)
 
 	// Col 5.
-	m[5][0] = grid.Cell(Dirt)
-	m[5][1] = grid.Cell(Dirt)
-	m[5][2] = grid.Cell(Dirt)
+	m[5][0] = grid.Cell(Gravel)
+	m[5][1] = grid.Cell(Gravel)
+	m[5][2] = grid.Cell(Gravel)
 	m[5][3] = grid.Cell(Grass)
 	m[5][4] = grid.Cell(Grass)
 	m[5][5] = grid.Cell(Grass)
-	m[5][6] = grid.Cell(Dirt)
-	m[5][7] = grid.Cell(Dirt)
-	m[5][8] = grid.Cell(Dirt)
+	m[5][6] = grid.Cell(Gravel)
+	m[5][7] = grid.Cell(Gravel)
+	m[5][8] = grid.Cell(Gravel)
 	m[5][9] = grid.Cell(Sand)
 	m[5][10] = grid.Cell(Water)
 
 	// Col 6.
-	m[6][0] = grid.Cell(Dirt)
-	m[6][1] = grid.Cell(Dirt)
-	m[6][2] = grid.Cell(Dirt)
+	m[6][0] = grid.Cell(Gravel)
+	m[6][1] = grid.Cell(Gravel)
+	m[6][2] = grid.Cell(Gravel)
 	m[6][3] = grid.Cell(Grass)
 	m[6][4] = grid.Cell(Grass)
 	m[6][5] = grid.Cell(Grass)
-	m[6][6] = grid.Cell(Dirt)
-	m[6][7] = grid.Cell(Dirt)
-	m[6][8] = grid.Cell(Dirt)
+	m[6][6] = grid.Cell(Gravel)
+	m[6][7] = grid.Cell(Gravel)
+	m[6][8] = grid.Cell(Gravel)
 	m[6][9] = grid.Cell(Sand)
 	m[6][10] = grid.Cell(Water)
 
 	// Col 7.
-	m[7][0] = grid.Cell(Dirt)
-	m[7][1] = grid.Cell(Dirt)
-	m[7][2] = grid.Cell(Dirt)
+	m[7][0] = grid.Cell(Gravel)
+	m[7][1] = grid.Cell(Gravel)
+	m[7][2] = grid.Cell(Gravel)
 	m[7][3] = grid.Cell(Grass)
 	m[7][4] = grid.Cell(Grass)
 	m[7][5] = grid.Cell(Grass)
-	m[7][6] = grid.Cell(Dirt)
-	m[7][7] = grid.Cell(Dirt)
-	m[7][8] = grid.Cell(Dirt)
+	m[7][6] = grid.Cell(Gravel)
+	m[7][7] = grid.Cell(Gravel)
+	m[7][8] = grid.Cell(Gravel)
 	m[7][9] = grid.Cell(Sand)
 	m[7][10] = grid.Cell(Water)
+
+	// Col 8.
+	m[8][0] = grid.Cell(Water)
+	m[8][1] = grid.Cell(Sand)
+	m[8][2] = grid.Cell(Water)
+	m[8][3] = grid.Cell(Grass)
+	m[8][4] = grid.Cell(Water)
+	m[8][5] = grid.Cell(Sand)
+	m[8][6] = grid.Cell(Water)
+	m[8][7] = grid.Cell(Grass)
+	m[8][8] = grid.Cell(Water)
+	m[8][9] = grid.Cell(Sand)
+	m[8][10] = grid.Cell(Water)
 }
